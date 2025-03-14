ea1ad006a3b58783399231bcb2d9111f430fe373
Sebastian <sebastianmarcelomora@gmail.com>
2025-03-14 12:26:16 -0300

    implementa la funcion para mostrar amigos

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ function agregarAmigo() {
     if(nombre == "") {
         alert("Por favor, inserte un nombre.")
     }else { //actualiza el array si es valido
-        amigos.push(nombre)
+        amigos.push(nombre);
     }
 
     //limpia el input
-    limpiarCaja()
+    limpiarCaja();
+
+    mostrarAmigos();
 
 }
 
@@ -21,3 +23,22 @@ function limpiarCaja() {
     document.getElementById("amigo").value = "";
 }
 
+function mostrarAmigos() {
+
+    //obtener el elemento <ul> donde se mostrarán los amigos
+    const lista = document.getElementById('listaAmigos');
+
+    //limpiar la lista antes de agregar los nuevos elementos
+    lista.innerHTML = "";
+
+    //usamos un bucle for para recorrer el array de amigos
+    for (let i = 0; i < amigos.length; i++) {
+    //crear un nuevo <li> para cada amigo
+    const li = document.createElement('li');
+    li.textContent = amigos[i];
+    
+    //añadir el <li> a la lista
+    lista.appendChild(li);
+    }
+
+}
